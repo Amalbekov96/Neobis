338cc30716907f9108dcd0337b6712ebdb5d77f0
Kushtar <39180239+Amalbekov96@users.noreply.github.com>
2020-02-29 15:07:22 +0600

    Update Migration.java

--- Weak7/Migration/Migration.java
+++ Weak7/Migration/Migration.java
@@ -4,7 +4,7 @@ import org.flywaydb.core.Flyway;
 public class Migrate_app {
     public static void main(String[] args) {
 
-        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/Nature?currentSchema=recods", "root", "mysql2019").load();
+        Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/Nature?currentSchema=recods", "root", "mysql").load();
         flyway.migrate();
     }
 }
